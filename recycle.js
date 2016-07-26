c0a78763de6caa02df7f9a1309f08d1e76796b01
Derek Detweiler <derek.detweiler@gmail.com>
2016-07-25 21:43:21 -0400

    Fixed argument offset due to binding.

--- recycle.js
+++ recycle.js
@@ -2,90 +2,9 @@
 (function (window, Object, Array) {
     'use strict';
     
-    var properties = {
-            setUp: {
-                value: null
-            },
-            recycle: {
-                value: null
-            }
-        },
-        recycleProd = function (cache, instance) {
-            cache.push(instance);
-        },
-        recycleDebug = function (cache, instance) {
-            if (instance.recycled) {
-                console.warn('WHOA! I have already been recycled!', instance);
-            } else {
-                instance.recycled = true;
-                cache.push(instance);
-            }
-        },
-        recycleProp = {
+    var recycleProp = {
             value: false,
             writable: true
-        },
-        setUpProd = function (cache) {
-            var newObject = null;
-            
-            if (cache.length) {
-                newObject = cache.pop();
-            } else {
-                newObject = Object.create(this.prototype);
-            }
-
-            this.apply(newObject, arguments);
-
-            return newObject;
-        },
-        setUpArrayProd = function (cache) {
-            var i = 0,
-                arr = null;
-            
-            if (cache.length) {
-                arr = cache.pop();
-            } else {
-                arr = [];
-            }
-            
-            for (i = 0; i < arguments.length; i++) {
-                arr.push(arguments[i]);
-            }
-
-            return arr;
-        },
-        setUpDebug = function (cache) {
-            var newObject = null;
-            
-            if (cache.length) {
-                newObject = cache.pop();
-                newObject.recycled = false;
-            } else {
-                newObject = Object.create(this.prototype);
-                Object.defineProperty(newObject, 'recycled', recycleProp);
-            }
-
-            this.apply(newObject, arguments);
-
-            return newObject;
-        },
-        setUpArrayDebug = function (cache) {
-            var i = 0,
-                arr = null;
-            
-            if (cache.length) {
-                arr = cache.pop();
-                arr.recycled = false;
-            } else {
-                arr = [];
-                Object.defineProperty(arr, 'recycled', recycleProp);
-            }
-            
-            for (i = 1; i < arguments.length; i++) {
-                arr.push(arguments[i]);
-            }
-
-            return arr;
         },
         caches = {},
         cacheIndex = 0;
@@ -101,15 +20,81 @@
 
             caches[name || ('cache-' + (cacheIndex++))] = cache;
             
-            if (debug) {
-                properties.setUp.value = (isArray ? setUpArrayDebug : setUpDebug).bind(ClassObject, cache);
-                properties.recycle.value = recycleDebug.bind(ClassObject, cache);
-            } else {
-                properties.setUp.value = (isArray ? setUpArrayProd : setUpProd).bind(ClassObject, cache);
-                properties.recycle.value = recycleProd.bind(ClassObject, cache);
-            }
+            Object.defineProperties(ClassObject, {
+                setUp: {
+                    value: (isArray ? (debug ? function () {
+                        var i = 0,
+                            arr = null;
+                        
+                        if (cache.length) {
+                            arr = cache.pop();
+                            arr.recycled = false;
+                        } else {
+                            arr = [];
+                            Object.defineProperty(arr, 'recycled', recycleProp);
+                        }
+                        
+                        for (i = 0; i < arguments.length; i++) {
+                            arr.push(arguments[i]);
+                        }
 
-            Object.defineProperties(ClassObject, properties);
+                        return arr;
+                    } : function () {
+                        var i = 0,
+                            arr = null;
+                        
+                        if (cache.length) {
+                            arr = cache.pop();
+                        } else {
+                            arr = [];
+                        }
+                        
+                        for (i = 0; i < arguments.length; i++) {
+                            arr.push(arguments[i]);
+                        }
+
+                        return arr;
+                    }) : (debug ? function () {
+                        var newObject = null;
+                        
+                        if (cache.length) {
+                            newObject = cache.pop();
+                            newObject.recycled = false;
+                        } else {
+                            newObject = Object.create(this.prototype);
+                            Object.defineProperty(newObject, 'recycled', recycleProp);
+                        }
+
+                        this.apply(newObject, arguments);
+
+                        return newObject;
+                    } : function () {
+                        var newObject = null;
+                        
+                        if (cache.length) {
+                            newObject = cache.pop();
+                        } else {
+                            newObject = Object.create(this.prototype);
+                        }
+
+                        this.apply(newObject, arguments);
+
+                        return newObject;
+                    }))
+                },
+                recycle: {
+                    value: (debug ? function (instance) {
+                        if (instance.recycled) {
+                            console.warn('WHOA! I have already been recycled!', instance);
+                        } else {
+                            instance.recycled = true;
+                            cache.push(instance);
+                        }
+                    } : function (instance) {
+                        cache.push(instance);
+                    })
+                }
+            });
             Object.defineProperty(ClassObject.prototype, 'recycle', {
                 value: recycle || (isArray ? function (depth) {
                     var i = 0;
